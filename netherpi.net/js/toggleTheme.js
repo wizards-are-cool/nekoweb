30f828610ac0fb795a931bcfbf996936e776a89f
wizard <netherpi@proton.me>
2026-01-08 16:19:40 -0500

    update

--- netherpi.net/js/toggleTheme.js
+++ netherpi.net/js/toggleTheme.js
@@ -1,17 +1,6 @@
 // modified from Emma Moore
 
 const theme_sw_butt = document.querySelector("#theme-toggler button");
-let theme1 = localStorage.getItem("theme");
-
-if (theme1 === "dark") {
-    if (theme_sw_butt)
-        theme_sw_butt.querySelector("span").innerHTML =
-            "<img class='pixel_but' src='/assets/img/sun.gif' alt='sun'/><span class='butt-text'>change theme</span>";
-} else {
-    if (theme_sw_butt)
-        theme_sw_butt.querySelector("span").innerHTML =
-            "<img class='pixel' src='/assets/img/moon.gif' alt='moon'/> <span class='butt-text'>change theme</span>";
-}
 
 theme_sw_butt.addEventListener("click", () => {
     let theme = localStorage.getItem("theme");
@@ -19,12 +8,12 @@ theme_sw_butt.addEventListener("click", () => {
         setDarkTheme();
         if (theme_sw_butt)
             theme_sw_butt.querySelector("span").innerHTML =
-                "<img class='pixel' src='/assets/img/sun.gif' alt='sun'/> <span class='butt-text'>change theme</span>";
+                "<img src='/assets/img/sun.gif' alt='sun'/> <span class='butt-text'>change theme</span>";
     } else {
         setLightTheme();
         if (theme_sw_butt)
             theme_sw_butt.querySelector("span").innerHTML =
-                "<img class='pixel' src='/assets/img/moon.gif' alt='moon'/> <span class='butt-text'>change theme</span>";
+                "<img src='/assets/img/moon.gif' alt='moon'/> <span class='butt-text'>change theme</span>";
     }
 });
 function setDarkTheme() {
