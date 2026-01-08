50ce6cc5b2e6b1e1bb079849ef93274464cee3c4
wizard <netherpi@proton.me>
2026-01-08 18:45:11 -0500

    update

---
+++ netherpi.net/js/checkTheme.js
@@ -0,0 +1,5 @@
+const darkPreference = window.matchMedia(
+    "(prefers-color-scheme: dark)",
+).matches;
+const theme = darkPreference ? "dark" : "light";
+document.documentElement.setAttribute("data-theme", theme);
--- netherpi.net/js/main.js
+++ netherpi.net/js/main.js
@@ -1,1 +1,6 @@
 console.log("Hi there!");
+const darkPreference = window.matchMedia(
+    "(prefers-color-scheme: dark)",
+).matches;
+const theme = darkPreference ? "dark" : "light";
+document.documentElement.setAttribute("data-theme", theme);
